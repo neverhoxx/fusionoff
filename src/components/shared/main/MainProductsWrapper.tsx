42a8a2766059a1a8e101b61f118ba377075e0fdc
neverhoxx <yehor.kiiakh@gmail.com>
2025-12-08 21:39:38 +0200

    asd

--- src/components/shared/main/MainProductsWrapper.tsx
+++ src/components/shared/main/MainProductsWrapper.tsx
@@ -4,7 +4,7 @@ import MainProducts from "./products";
 export default async function MainProductsWrapper() {
     const products = await prisma.product.findMany({
         include: { images: true },
-        orderBy: { createdAt: "desc" },
+        orderBy: { createdAt: "desc" }
     });
 
     return <MainProducts products={products} />;
